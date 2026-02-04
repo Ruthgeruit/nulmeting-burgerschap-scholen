1bdbbd57b0ccafc0721072d61b8e55097f9dc656
Ruthgeruit <info@ruthgeruiterwijk.nl>
2026-02-04 14:19:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import NulmetingKubus from './NulmetingKubus.jsx'
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <NulmetingBurgerschap />
+      <NulmetingKubus />
     </div>
   );
 }
+
 
 
 
